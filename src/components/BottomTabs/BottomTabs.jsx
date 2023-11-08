4ee22f4f1890a574d90766afb8faa721ded4534f
Sreerag Pavithran <sreeragpavithran@gmail.com>
2023-11-08 14:28:34 +0530

    PWA Button Function

--- src/components/BottomTabs/BottomTabs.jsx
+++ src/components/BottomTabs/BottomTabs.jsx
@@ -1,24 +1,57 @@
 // import React from 'react'
 
-import {
-  BiCloudDownload,
-  BiHeart,
-  BiHomeAlt2,
-  BiMessageRounded,
-  BiPlus,
-} from "react-icons/bi";
+import { BiHeart, BiHomeAlt2, BiMessageRounded, BiPlus } from "react-icons/bi";
 import { LuMonitorSmartphone } from "react-icons/lu";
 import { BottomNavigation } from "reactjs-bottom-navigation";
+import { useEffect, useState } from "react";
 
 import "./style.css";
 
 const BottomTabs = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setDeferredPrompt(event);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("PWA installed");
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
   const tabs = [
     { key: "home", icon: <BiHomeAlt2 style={{ fontSize: 18 }} /> },
     { key: "saved", icon: <BiHeart style={{ fontSize: 18 }} /> },
     { key: "add", icon: <BiPlus style={{ fontSize: 22 }} /> },
     { key: "message", icon: <BiMessageRounded style={{ fontSize: 18 }} /> },
-    { key: "install", icon: <LuMonitorSmartphone style={{ fontSize: 20 }} /> },
+    {
+      key: "install",
+      icon: (
+        <LuMonitorSmartphone
+          style={{ fontSize: 20 }}
+          onClick={handleInstallClick}
+        />
+      ),
+    },
   ];
   return (
     <div>
